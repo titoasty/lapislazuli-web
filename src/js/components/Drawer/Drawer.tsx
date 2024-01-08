1e48d12ded062dbf718788580f22b1c81ea47e29
nico b <bouvet.nicolas@gmail.com>
2024-01-08 17:30:27 +0100

    drawer wip

--- src/js/components/Drawer/Drawer.tsx
+++ src/js/components/Drawer/Drawer.tsx
@@ -1,5 +1,50 @@
+import classNames from 'classnames';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import styles from './styles.module.scss';
 
-export function Drawer() {
-    return <div className={styles.drawer}></div>
+export function DrawerTitle({ className, children, ...props }: any) {
+    return (
+        <div className={classNames(styles.title, className)} {...props}>
+            {children}
+        </div>
+    );
+}
+
+export function DrawerClose({ className, children, ...props }: any) {
+    return (
+        <div className={classNames(styles.btnClose, className)} {...props}>
+            {children}
+        </div>
+    );
+}
+
+export function Drawer({ visible, hideCloseBtn, close, className, children, ...props }: any) {
+    console.log('visible', visible);
+
+    useEffect(() => {
+        if (!visible) {
+            return;
+        }
+
+        const onKeyDown = (evt: KeyboardEvent) => {
+            if (evt.key === 'Escape') {
+                close?.();
+            }
+        };
+
+        window.addEventListener('keydown', onKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [visible]);
+
+    return createPortal(
+        <div className={classNames(styles.drawer, !visible && styles.drawer_hidden, className)} {...props}>
+            {!hideCloseBtn && <DrawerClose onClick={close} />}
+            <div className={styles.content}>{children}</div>
+        </div>,
+        document.body
+    );
 }
--- src/js/pages/Events/Events.tsx
+++ src/js/pages/Events/Events.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
 
+import { Drawer, DrawerTitle } from 'components/Drawer/Drawer';
 import useWindowSize from 'hooks/useWindowSize';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -17,6 +18,7 @@ export function Events() {
     const introRef = useRef<HTMLDivElement>(null);
     const windowSize = useWindowSize();
     const [swiperInst, setSwiperInst] = useState<unknown>();
+    const [drawerVisible, setDrawerVisible] = useState(true);
 
     useEffect(() => {
         if (!swiperInst) {
@@ -57,6 +59,14 @@ export function Events() {
         bottomRef.current!.style.height = `${height}px`;
     }, [windowSize]);
 
+    const openDrawer = useCallback(() => {
+        setDrawerVisible(true);
+    }, []);
+
+    const closeDrawer = useCallback(() => {
+        setDrawerVisible(false);
+    }, []);
+
     return (
         <div className={styles.page_events}>
             <Swiper //
@@ -76,7 +86,7 @@ export function Events() {
                 onSetTransition={onSetTransition}
             >
                 <SwiperSlide className={styles.slide}>
-                    <div className={styles.content}>
+                    <div className={styles.content} onClick={openDrawer}>
                         <div className={styles.image} />
                         <div className={styles.type}>Exhibition</div>
                     </div>
@@ -89,13 +99,13 @@ export function Events() {
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className={styles.slide}>
-                    <div className={styles.content}>
+                    <div className={styles.content} onClick={openDrawer}>
                         <div className={styles.image} />
                         <div className={styles.type}>Fair</div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className={styles.slide}>
-                    <div className={styles.content}>
+                    <div className={styles.content} onClick={openDrawer}>
                         <div className={styles.image} />
                         <div className={styles.type}>Residency</div>
                     </div>
@@ -114,6 +124,18 @@ export function Events() {
                     <div className={styles.text}>SCROLL TO EXPLORE</div>
                 </div>
             </div>
+            <Drawer visible={drawerVisible} close={closeDrawer}>
+                <DrawerTitle>H2o Water Diaries</DrawerTitle>
+                <div>
+                    Japanese contemporary artist Takashi Murakami is a master of mediums, working with painting, sculpture, fashion, film, and even animation. His influence on Japan equals that of Andy Warhol on America. However, the art of Takashi
+                    Murakami unites Japanese and Western elements to create unique, fantastical worlds that are cute and scary. When Takashi Murakami invented the term Superflat in 2001, he launched one of postmodern art's most exciting and refreshing art
+                    movements. The artist is influenced by the Post World War II manga and anime craze. Superflat references both the two-dimensional quality of traditional Japanese painting and the shallow qualities of consumer culture. As a result,
+                    common motifs and subjects flood the oeuvre of Takashi Murakami, like flowers, skulls, bests, and bears. You can see them across paintings, wallpaper, pillows, Louis Vuitton bags, and NFTs! As a pioneer in the field of NFTs, Murakami
+                    continues to be curious about all the possibilities in art. As a result, common motifs and subjects flood the oeuvre of Takashi Murakami, like flowers, skulls, bests, and bears. You can see them across paintings, wallpaper, pillows,
+                    Louis Vuitton bags, and NFTs! As a pioneer in the field of NFTs, Murakami continues to be curious about all the possibilities in art. As a result, common motifs and subjects flood the oeuvre of Takashi Murakami, like flowers, skulls,
+                    bests, and bears. You can see them across paintings, wallpaper, pillows, Louis Vuitton bags, and NFTs! As a pioneer in the field of NFTs, Murakami continues to be curious about all the possibilities in art.
+                </div>
+            </Drawer>
         </div>
     );
 }
